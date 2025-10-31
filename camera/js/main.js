99ccdfe9158c68ecca03e977235de517f6995860
bragaxs <160557004+bragaxs@users.noreply.github.com>
2025-10-31 16:23:37 -0400

    commit - vercel

---
+++ camera/js/main.js
@@ -0,0 +1,46 @@
+// registrando o service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+      console.log('Service worker registrado! 😎', reg);
+    } catch (err) {
+      console.log('😢 Service worker registro falhou: ', err);
+    }
+  });
+}
+
+// configurando as constraints do video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+// capturando os elementos em tela
+const cameraView = document.querySelector("#camera--view"),
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger");
+
+// Estabelecendo o acesso à câmera e inicializando a visualização
+function cameraStart() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      let track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.error("Ocorreu um erro.", error);
+    });
+}
+
+// Função para tirar foto
+cameraTrigger.onclick = function () {
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+};
+
+// Carrega imagem de câmera quando a janela carregar
+window.addEventListener("load", cameraStart, false);
