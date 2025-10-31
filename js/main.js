fd725690f06bd9193f585ebdbea8ec3bc30c137f
bragaxs <160557004+bragaxs@users.noreply.github.com>
2025-10-31 17:20:21 -0400

    ...

--- js/main.js
+++ js/main.js
@@ -51,11 +51,8 @@ cameraTrigger.onclick = function () {
 };
 
 trocarCam.onclick = function () {
-  cameraSensor.width = cameraView.videoWidth;
-  cameraSensor.height = cameraView.videoHeight;
-  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-  cameraOutput.src = cameraSensor.toDataURL("image/webp");
-  cameraOutput.classList.add("taken");
+  toggleCamera();
+  cameraStart();
 }
 
 // Carrega imagem de câmera quando a janela carregar
